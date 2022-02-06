982cfa041328ed0ddc6278138df472263812b9b5
padmar22 <padmarajiv22@gmail.com>
2022-02-06 18:59:18 +0530

    new code in b2

--- src/test/java/org/maven/first_maven1/Sample.java
+++ src/test/java/org/maven/first_maven1/Sample.java
@@ -4,8 +4,8 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("Test1");
-		System.out.println("Test2");
-		System.out.println("Test3");
+		System.out.println("Test4");
+		System.out.println("Test5");
 	}
 
 }
